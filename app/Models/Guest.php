c1d3ce9467a72c2c9213b53c6a82b075626b6b61
AmirHossein Najarbshi <amir.hossen.nb@gmail.com>
2025-02-25 19:50:40 +0330

    add guest api resources

--- app/Models/Guest.php
+++ app/Models/Guest.php
@@ -5,6 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @OA\Schema(
+ *     schema="Guest",
+ *     type="object",
+ *     required={"first_name", "last_name"},
+ *     @OA\Property(property="id", type="integer", description="Guest ID"),
+ *     @OA\Property(property="first_name", type="string", description="First name of the guest"),
+ *     @OA\Property(property="last_name", type="string", description="Last name of the guest"),
+ *     @OA\Property(property="created_at", type="string", format="date-time"),
+ *     @OA\Property(property="updated_at", type="string", format="date-time")
+ * )
+ */
 class Guest extends Model
 {
     /** @use HasFactory<\Database\Factories\GuestFactory> */

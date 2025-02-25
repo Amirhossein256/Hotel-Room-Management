b5052af6f940641a3fb0c2ae41192f54bf232f57
AmirHossein Najarbshi <amir.hossen.nb@gmail.com>
2025-02-25 18:31:03 +0330

    add guest database structure

---
+++ app/Models/Guest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Guest extends Model
+{
+    /** @use HasFactory<\Database\Factories\GuestFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'last_name',
+        'first_name'
+    ];
+}

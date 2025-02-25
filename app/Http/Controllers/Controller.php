4be0f49c5e338d4643615023b6faa46b7a6f1a6f
AmirHossein Najarbshi <amir.hossen.nb@gmail.com>
2025-02-25 19:51:55 +0330

    fix swagger info

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,16 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *     title="Hotel Management API",
+ *     version="1.0.0",
+ *     description="API documentation for managing hotel rooms, guests, and reservations",
+ *     @OA\Contact(
+ *         email="support@example.com"
+ *     )
+ * )
+ */
 abstract class Controller
 {
     //
